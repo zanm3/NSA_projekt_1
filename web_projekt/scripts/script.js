bd37b0f262c2f59cc1430667d62a9e45dfc06e01
zanm3 <zan.markuza@tscng.org>
2024-10-30 17:18:34 +0100

    Add files via upload

--- web_projekt/scripts/script.js
+++ web_projekt/scripts/script.js
@@ -1,21 +1,37 @@
-function showHidden(){
+function showHidden() {
     const stevilo = (document.querySelector('input[name="steviloOseb"]:checked')).value;
     const hiddenDiv = document.getElementById("steviloNum");
-    if(stevilo === "vecosebna"){
+    if (stevilo === "vecosebna") {
         hiddenDiv.style.display = "flex";
-    }else{
+    } else {
         hiddenDiv.style.display = "none";
     }
 }
-function showHidden2(){
+function showHidden2() {
     const smer = (document.querySelector('input[name="smer"]:checked')).value;
     const hiddenDiv = document.getElementById("prihod");
     let b;
-    if(smer === "povratni"){
+    if (smer === "povratni") {
         hiddenDiv.style.display = "block";
         b = true;
-    }else{
+    } else {
         hiddenDiv.style.display = "none";
         b = false;
     }
+}
+function uspesno() {
+    const form = document.getElementById("forma");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Super!',
+            text: 'Nakup vozovnice je bil uspešen'
+
+        }).then(() => {
+            form.submit();
+        });
+    });
 }
